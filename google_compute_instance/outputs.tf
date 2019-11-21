9a3e4d4d8895abe4e9da8e9e92123d26b36365ac
Sean Walker <45837093+seanwalker5@users.noreply.github.com>
2019-11-21 11:10:28 -0500

    Update outputs.tf
    Uncommented "external_ip". Changed "assigned_nat_ip" to "nat_ip"

--- google_compute_instance/outputs.tf
+++ google_compute_instance/outputs.tf
@@ -1,6 +1,6 @@
-#output "external_ip" {
-#  value = google_compute_instance.demo[*].network_interface.0.access_config.0.assigned_nat_ip
-#}
+output "external_ip" {
+  value = google_compute_instance.demo[*].network_interface.0.access_config.0.nat_ip
+}
 
 output "internal_ip" {
   value = google_compute_instance.demo[*].network_interface.0.network_ip
